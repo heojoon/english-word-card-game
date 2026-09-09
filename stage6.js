5c3e6db1067c050b0d275cd23dd5a8c49cf540ba
Joon Heo <heojoon48@gmail.com>
2026-09-09 16:04:11 +0900

    Add Stage 6 Day 15 vocabulary

---
+++ stage6.js
@@ -0,0 +1,37 @@
+window.QUIZ_STAGES=window.QUIZ_STAGES||{};
+window.QUIZ_STAGES.s6={
+  name:'Stage 6',
+  desc:'Day 15 · 30문제',
+  words:[
+    ['argument','명','논쟁, 말다툼'],
+    ['drugstore','명','약국'],
+    ['repetition','명','반복'],
+    ['spin','동','돌다, 회전시키다'],
+    ['director','명','지도자, 책임자'],
+    ['dialect','명','사투리, 방언'],
+    ['rate','명','비율'],
+    ['project','명','계획, 연구과제, 사업계획'],
+    ['personality','명','성격, 인격'],
+    ['available','형','구할 수 있는, 여유가 있는'],
+    ['bump','동','~에 부딪치다'],
+    ['well-known','형','유명한, 잘 알려진'],
+    ['flow','명/동','흐름; 흐르다'],
+    ['waterproof','형','방수의'],
+    ['restore','동','복구하다'],
+    ['secondary','형','제2의, 중등 교육의'],
+    ['relative','명','친척'],
+    ['slippery','형','미끄러운, 미끈거리는'],
+    ['fix','동','고정하다'],
+    ['get rid of','숙어','~을 제거하다, 없애다'],
+    ['mention','동','언급하다'],
+    ['announcement','명','발표, 소식'],
+    ['chop','동','(음식을) 다지다, 썰다'],
+    ['peasant','명','농부, 소작농'],
+    ['portable','형','휴대하기 쉬운, 휴대용의'],
+    ['survive','동','살아남다'],
+    ['erase','동','지우다'],
+    ['migrant','명','이주자'],
+    ['mince','동','(고기 등을) 갈다, 잘게 썰다'],
+    ['pandemic','명/형','(전국적인) 유행병; 유행성의']
+  ]
+};
